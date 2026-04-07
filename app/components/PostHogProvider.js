cae530407c157f2cb688cd12c53135d7b68d6b47
run26long <run26long@cfl.rr.com>
2026-04-06 23:36:40 -0400

    Posthog update
    updates

--- app/components/PostHogProvider.js
+++ app/components/PostHogProvider.js
@@ -6,7 +6,7 @@ import { useEffect } from 'react'
 
 export function PostHogProvider({ children }) {
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
       capture_pageview: true,
       capture_pageleave: true,
