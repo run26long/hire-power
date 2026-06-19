afc3011c529ac784ed3fac3ddd7eeb9b3865f066
run26long <run26long@cfl.rr.com>
2026-06-19 11:37:13 -0400

    fix push harder button
    per christian's fix list

--- app/resume/[id]/page.js
+++ app/resume/[id]/page.js
@@ -4264,7 +4264,7 @@ function ImproveStep({ rewrittenResume, resumeChanges, setRewrittenResume, setRe
         )}
 
         {/* Buttons */}
-        <div className="flex gap-2 justify-center pt-1">
+        <div className="flex gap-2 justify-center pt-1 flex-wrap">
           {showPushHarder && (
             <button
               onClick={() => setShowGapsModal(true)}
